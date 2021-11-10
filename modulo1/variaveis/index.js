2cf1b3086db9a06992b51b58bd80ce9710d31ab1
marceloprestes <barbosaprestes83@gmail.com>
2021-11-10 20:28:03 -0300

    Exercicios

--- modulo1/variaveis/index.js
+++ modulo1/variaveis/index.js
@@ -1,4 +1,4 @@
-
+// Exercicios de variaveis
 // - //Exercícios de interpretação de código
     
 //     //Tente responder os exercícios dessa seção sem executar o código. Se ficar muito difícil, pode rodar no seu computador **para analisar e pensar sobre o resultado.** 
