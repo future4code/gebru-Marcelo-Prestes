5ebc842ead2a50bf2b46503f80dca06ba3126c22
Marcelo.Prestes <barbosaprestes83@gmail.com>
2022-03-13 23:43:53 -0300

    Componentes separados

--- projeto-labefy/labefy-app/src/App.js
+++ projeto-labefy/labefy-app/src/App.js
@@ -1,134 +1,48 @@
-/* eslint-disable react/jsx-no-undef */
-import "./styles.css";
-import axios from "axios";
-import React from "react";
+import React from 'react';
+import SigUpPlayListScreen from './components/SigUpPlayListScreen'
+import PlaylistScnreen from './components/PlaylistScnreen'
+import axios from 'axios'
 
+class App extends React.Component {
+   state = {
+       renderScreen:  'signUp'
+   }
+   
+   chooseScreen = () => {
+       switch (this.state.renderScreen) {
+           case 'signUp':
+              return <SigUpPlayListScreen gotoPlaylistScnreen={this.gotoPlaylistScnreen}/>
+               //return <SigUpPlayListScreen goToSigUpPlayListScreen={this.goToSigUpPlayListScreen}/>
+            case 'Playlist':
+                return  <PlaylistScnreen goToSigUpPlayListScreen={this.gotoPlaylistScnreen}/>
+            default:
+                return <h2>Erro</h2>
+       }
+   }
 
-const urlPlaylist =
-  "https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists";
-
-const headers = {
-  headers: {
-    Authorization: "marcelo-prestes-gebru"
+  _goToSigUpPlayListScreen = () => {
+    this.setState({ renderScreen: 'signUp' });
+  };
+  get goToSigUpPlayListScreen() {
+    return this._goToSigUpPlayListScreen;
   }
-};
-
-export default class App extends React.Component {
-
-  state = {
-    playlists: [],
-    playlistInput: "",
-    playlistIdInput: "" 
-
-  };
-
-
-  componentDidMount() {
-    this.getAllPlaylists();
-  }
-
-
-  getAllPlaylists = () => {
-    axios
-      .get(urlPlaylist, headers)
-      .then((res) => {
-
-
-        this.setState({ playlists: res.data.result.list });
-      })
-      .catch((err) => {
-        console.log(err.response);
-      });
-  };
-
-
-  createPlaylist = () => {
-
-    const body = {
-      name: this.state.playlistInput
-    };
-    axios
-      .post(urlPlaylist, body, headers)
-      .then((res) => {
-        console.log(res.data.result.list);
-        
-        alert(`A playlist ${this.state.playlistInput} foi criada com sucesso`);
-        
-        this.setState({ playlistInput: "" });
-        
-        this.getAllPlaylists();
-      })
-      
-      .catch((err) => {
-        console.log(err.response.data.message);
-      });
-  };
-
-  
-
-  onPlaylistTextChange = (event) => {
-    this.setState({ playlistInput: event.target.value });
-  };
-
-  deletePlaylist = async (playlistId) => {
-    try {
-      // eslint-disable-next-line no-unused-vars
-      const res = await axios.delete(
-        `https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${playlistId}`, {
-        headers: {
-          Authorization: 'marcelo-prestes-gebru'
-        }
-      })
-
-      alert('PlayList deletade com sucesso!')
-      this.getAllPlaylists()
-    } catch (err) {
-      console.log(err.response)
-    };
+  set goToSigUpPlayListScreen(value) {
+    this._goToSigUpPlayListScreen = value;
   }
 
-
-
-
-  onPlaylisIDtTextChange = (event) => {
-    this.setState({ playlistIdInput: event.target.value });
-  };
-  render() {
-    
-    const playlistComponents = this.state.playlists.map((play) => {
-      return (
-
-        <ListCard key={play.id}>
-        <span>{play.name}</span>
-        <button onClick={() => this.deletePlaylist(play.id)}>Delete {play.name}</button>
-      </ListCard>
-
-      );
-    });
-
-    return (
-      <div>
-        <h1>LabeFay </h1>
-        <input
-         
-          value={this.state.playlistInput}
-          placeholder="Digite a sua Playlist"
-          onChange={this.onPlaylistTextChange}
-        />
-
-       <button onClick={this.createPlaylist}>Criar</button>
-
-       
-        <div>
-          <input
-            value={this.state.playlistIdInput}
-            placeholder="Digite o ID da Playlist"
-            onChange={this.onPlaylisIDtTextChange}
-          />
-          <button onClick={this.deletePlaylist}>Deletar</button>
-          {playlistComponents}
-        </div>
-      </div>
-    );
-  }
+goToPlaylistScnreen = () => {
+    this.setState({renderScreen:'Playlist'})
 }
+
+    render() {
+        return (
+            <div>
+        <h1>LabeFay</h1>       
+        {this.chooseScreen()}
+        </div>
+        )
+    }
+}
+
+
+export default App
---
+++ projeto-labefy/labefy-app/src/components/telaPlay.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import axios from 'axios'
+import styled from 'styled-components'
+
+class telaPlay extends React.Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default telaPlay
