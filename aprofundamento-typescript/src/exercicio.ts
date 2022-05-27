9711e1ea138bbaf6ad2482a787b681245680f83e
Marcelo.Prestes <barbosaprestes83@gmail.com>
2022-05-26 23:36:06 -0300

    ts

---
+++ aprofundamento-typescript/src/exercicio.ts
@@ -0,0 +1,31 @@
+import { nodeModuleNameResolver } from "typescript"
+
+// exercicio 1
+const minhaString: string = "Trinta e oito"
+const meuNumero: number = 38
+
+
+const person :{name:string, age:number, colourFavourite:string} = {
+    name: "Marcelo",
+    age: 34,
+    colourFavourite: "Brown"
+}
+
+
+enum CorArcoIris {
+    VERMELHO: "Vermelho",
+    LARANJA: "Laranja",
+    AMARELO: "Amarelo",
+    VERDE: "Verde",
+    AZUL: "Azul",
+    ANIL: "Anil",
+    VIOLETA: "Violeta"
+}
+    const personD:{name:string, age:number,} = {
+        name: "Marcio",
+        age: 34,
+        class: CorArcoIris.AZUL
+    }
+    
+
+// exercicio 2
