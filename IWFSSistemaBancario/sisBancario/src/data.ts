fb40cc73bb7bc8f4c14a64bb9a873c59c4c0c2ca
Marcelo.Prestes <barbosaprestes83@gmail.com>
2022-06-10 22:10:48 -0300

    tipos contas

--- IWFSSistemaBancario/sisBancario/src/data.ts
+++ IWFSSistemaBancario/sisBancario/src/data.ts
@@ -1,14 +1,14 @@
-
-export enum CLIENT_TYPE{
-    ADMIN = "ADMIN",
-    NORMAL = "NORMAL"
+export enum ACCOUNT_TYPE{
+    JURIDICA = "JURIDICA",
+    FISICA = "FISICA"
 }
 
 export type Client = {
     id: number, 
+    cpf: number,
     name: string, 
     email: string, 
-    type: CLIENT_TYPE,
+    type: ACCOUNT_TYPE,
     age: number
 }
 
@@ -18,43 +18,49 @@ export let clients :Client[] = [
     {
         id: 1,
         name: "Alice",
+        cpf:,
         email: "alice@email.com",
-        type: CLIENT_TYPE.ADMIN,
+        type: ACCOUNT_TYPE.FISICA,
         age: 12,
     },
     {
         id: 2,
         name: "Bob",
+        cpf:,
         email: "bob@email.com",
-        type: CLIENT_TYPE.ADMIN,
+        type: ACCOUNT_TYPE.FISICA,
         age: 36,
     },
     {
         id: 3,
         name: "Roberto",
+        cpf:,
         email: "roberto@email.com",
-        type: CLIENT_TYPE.ADMIN,
+        type: ACCOUNT_TYPE.FISICA,
         age: 21,
     },
     {
         id: 4,
         name: "Ronaldo",
+        cpf:,
         email: "ronaldo@email.com",
-        type: CLIENT_TYPE.NORMAL,
+        type: ACCOUNT_TYPE.FISICA,
         age: 17,
     },
     {
         id: 5,
         name: "Marta",
+        cpf:,
         email: "marta@email.com",
-        type: CLIENT_TYPE.ADMIN,
+        type: ACCOUNT_TYPE.FISICA,
         age: 17,
     },
     {
         id: 6,
         name: "Fred",
+        cpf:,
         email: "fred@email.com",
-        type: CLIENT_TYPE.NORMAL,
+        type: ACCOUNT_TYPE.FISICA,
         age: 60,
     },
 ]
