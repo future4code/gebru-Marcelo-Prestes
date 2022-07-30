c8db44744027cf3a9bc704f10274fe2e552dc5bd
Marcelo Barbosa Prestes <marcelo.prestes@linx.com.br>
2022-07-29 21:09:53 -0300

    LaBook

--- labeflix/src/data/UserDatabase.ts
+++ labeflix/src/data/UserDatabase.ts
@@ -1,16 +1,24 @@
+import { User } from "../types/User";
 import { BaseDatabase } from "./BaseDatabase";
 
 export class UserDatabase extends BaseDatabase {
   private static TABLE_NAME = "LABEFLIX_USER";
 
-  async create({ id, name, email, password }: any): Promise<void> {
-    await UserDatabase.connection
-      .insert({
-        id,
-        name,
-        email,
-        password,
+
+  public insertUser = async (
+    user: User
+    ) => {
+      try{
+        await UserDatabase.connection.insert({
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          password: user.password,
       })
-      .into(UserDatabase.TABLE_NAME);
-  }
+      .into(this.TABLE_NAME);
+    } catch (error:any) {
+      throw new Error(error.message)
+   }
+
+}
 }
